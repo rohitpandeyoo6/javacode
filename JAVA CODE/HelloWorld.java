c18d8df2ea4abf045ba976e47f9ad05729e71be1
rohitpandeyoo6 <98679341+rohitpandeyoo6@users.noreply.github.com>
2023-01-20 00:11:22 +0530

    Commit directly from vs CODE

--- JAVA CODE/HelloWorld.java
+++ JAVA CODE/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello Rohit");
         System.out.println("Welcome rohit to new git repo of java");
+        System.out.println(100);
         
     }
 }
