a4aa7c701a00d68e4ee117953b5e99c14395b27d
rohitpandeyoo6 <98679341+rohitpandeyoo6@users.noreply.github.com>
2023-01-23 22:52:24 +0530

    factorial

---
+++ JAVA CODE/javacode/FactorialOfNumber.java
@@ -0,0 +1,13 @@
+package javacode;
+
+public class FactorialOfNumber {
+	public static void main(String[] args) {
+		int a=10;
+		int factorial=1;
+		for (int i = 1; i < 10; i++) {
+			factorial=factorial*i;
+			
+		}
+		System.out.println(factorial);
+	}
+}
