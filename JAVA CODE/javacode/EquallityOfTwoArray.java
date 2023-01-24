694e96c1c866cb836dc8154c4762f76ba0cc2d30
rohitpandeyoo6 <98679341+rohitpandeyoo6@users.noreply.github.com>
2023-01-24 23:00:02 +0530

    EqualityOfArray

--- JAVA CODE/javacode/EquallityOfTwoArray.java
+++ JAVA CODE/javacode/EquallityOfTwoArray.java
@@ -1,10 +1,17 @@
 package javacode;
 
+import java.util.Arrays;
+
 public class EquallityOfTwoArray {
 
 	public static void main(String[] args) {
-		int a[]= {}
-
+		int a[]= {1,2,3,4,4,6};
+		int b[]= {1,2,3,4,4,6};
+		/*
+		 * if (a.length==b.length) { System.out.println("arrays are equal"); } else {
+		 * System.out.println("arrays ane not equal"); }
+		 */
+		System.out.println(Arrays.equals(a, b));
 	}
 
 }
