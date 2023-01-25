e6b3ed7c4efdb66be06f21561cfa632ef0f89dea
rohitpandeyoo6 <98679341+rohitpandeyoo6@users.noreply.github.com>
2023-01-25 22:36:29 +0530

    Linear Search

---
+++ JAVA CODE/javacode/SearchingAnElement.java
@@ -0,0 +1,16 @@
+package javacode;
+//Linear Search
+public class SearchingAnElement {
+
+	public static void main(String[] args) {
+		int a[]= {2,4,77,33,45,34};
+		int target=45;
+		for (int i = 0; i < a.length; i++) {
+			if (target==a[i]) {
+				System.out.println("elememt is present");
+			} 
+			
+		}
+	}
+
+}
